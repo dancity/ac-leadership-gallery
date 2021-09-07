5ecf4d6b072090580a9c1ae0dd6ffda002ed5cd6
dancity <matheusleaorangel@gmail.com>
2021-09-07 20:36:45 -0300

    start
    Commit start

---
+++ src/photos.js
@@ -0,0 +1,367 @@
+import { auto } from "async";
+import posse1 from "./photos/posse1.jpg";
+import posse2 from "./photos/posse2.jpg";
+import posse3 from "./photos/posse3.jpg";
+import posse4 from "./photos/posse4.jpg";
+import posse5 from "./photos/posse5.jpg";
+import posse6 from "./photos/posse6.jpg";
+import posse7 from "./photos/posse7.jpg";
+import posse8 from "./photos/posse8.jpg";
+import posse9 from "./photos/posse9.jpg";
+import posse10 from "./photos/posse10.jpg";
+import posse11 from "./photos/posse11.jpg";
+import posse12 from "./photos/posse12.jpg";
+import posse13 from "./photos/posse13.jpg";
+import posse14 from "./photos/posse14.jpg";
+import posse15 from "./photos/posse15.jpg";
+import posse16 from "./photos/posse16.jpg";
+import posse17 from "./photos/posse17.jpg";
+import posse18 from "./photos/posse18.jpg";
+import posse19 from "./photos/posse19.jpg";
+import posse20 from "./photos/posse20.jpg";
+import posse21 from "./photos/posse21.jpg";
+import posse22 from "./photos/posse22.jpg";
+import posse23 from "./photos/posse23.jpg";
+import posse24 from "./photos/posse24.jpg";
+import posse25 from "./photos/posse25.jpg";
+import posse26 from "./photos/posse26.jpg";
+import posse27 from "./photos/posse27.jpg";
+import posse28 from "./photos/posse28.jpg";
+import posse29 from "./photos/posse29.jpg";
+import posse30 from "./photos/posse30.jpg";
+import posse31 from "./photos/posse31.jpg";
+import posse32 from "./photos/posse32.jpg";
+import posse33 from "./photos/posse33.jpg";
+import posse34 from "./photos/posse34.jpg";
+import posse35 from "./photos/posse35.jpg";
+import posse36 from "./photos/posse36.jpg";
+import posse37 from "./photos/posse37.jpg";
+import posse38 from "./photos/posse38.jpg";
+import posse39 from "./photos/posse39.jpg";
+import posse40 from "./photos/posse40.jpg";
+import posse41 from "./photos/posse41.jpg";
+import posse42 from "./photos/posse42.jpg";
+import posse43 from "./photos/posse43.jpg";
+import posse44 from "./photos/posse44.jpg";
+import posse45 from "./photos/posse45.jpg";
+import posse46 from "./photos/posse46.jpg";
+import posse47 from "./photos/posse47.jpg";
+import posse48 from "./photos/posse48.jpg";
+import posse49 from "./photos/posse49.jpg";
+import posse50 from "./photos/posse50.jpg";
+import posse51 from "./photos/posse51.jpg";
+import posse52 from "./photos/posse52.jpg";
+import posse53 from "./photos/posse53.jpg";
+import posse54 from "./photos/posse54.jpg";
+import posse55 from "./photos/posse55.jpg";
+import posse56 from "./photos/posse56.jpg";
+import posse57 from "./photos/posse57.jpg";
+import posse58 from "./photos/posse58.jpg";
+import posse59 from "./photos/posse59.jpg";
+import posse60 from "./photos/posse60.jpg";
+
+
+
+export const photos = [
+    {
+      src: posse1,
+      width: 5,
+      height: 4,
+    },
+    {
+      src: posse2,
+      width: 5,
+      height: 4,
+    },
+    {
+      src: posse3,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse4,
+      width: 6,
+      height: 4
+    },
+    {
+      src: posse5,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse6,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse7,
+      width: 6,
+      height: 4
+    },
+    {
+      src: posse8,
+      width: 4,
+      height: 3
+    },
+    {
+      src: posse9,
+      width: 4,
+      height: 3
+    },
+    {
+      src: posse10,
+      width: 5,
+      height: 4,
+    },
+    {
+      src: posse11,
+      width: 4,
+      height: 4,
+    },
+    {
+      src: posse12,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse13,
+      width: 6,
+      height: 4
+    },
+    {
+      src: posse14,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse15,
+      width: 3,
+      height: 5
+    },
+    {
+      src: posse16,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse17,
+      width: 4,
+      height: 4
+    },
+    {
+      src: posse18,
+      width: 4,
+      height: 3
+    },
+    {
+      src: posse19,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse20,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse21,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse22,
+      width: 5,
+      height: 4
+    },
+    {
+      src: posse23,
+      width: 7,
+      height: 4
+    },
+    {
+      src: posse24,
+      width: 4,
+      height: 4
+    },
+    {
+      src: posse25,
+      width: 4,
+      height: 5
+    },
+    {
+      src: posse26,
+      width: 5,
+      height: 3
+    },
+    {
+      src: posse27,
+      width: 4,
+      height: 5
+    },
+    {
+      src: posse28,
+      width: 4,
+      height: 4
+    },
+    {
+      src: posse29,
+      width: 3,
+      height: 5
+    },
+    {
+      src: posse30,
+      width: 5,
+      height: 5
+    },
+    {
+      src: posse31,
+      width: 6,
+      height: 5
+    },
+    {
+      src: posse32,
+      width: 3,
+      height: 4
+    },
+    {
+      src: posse33,
+      width: 6,
+      height: 5
+    },
+    {
+      src: posse34,
+      width: 6,
+      height: 5
+    },
+    {
+      src: posse35,
+      width: 3,
+      height: 4
+    },
+    {
+      src: posse36,
+      width: 6,
+      height: 4
+    },
+    {
+      src: posse37,
+      width: 6,
+      height: 5
+    },
+    {
+      src: posse38,
+      width: 6,
+      height: 5
+    },
+    {
+      src: posse39,
+      width: 5,
+      height: 5
+    },
+    {
+      src: posse40,
+      width: 3,
+      height: 4
+    },
+    {
+      src: posse41,
+      width: 4,
+      height: 4
+    },
+    {
+      src: posse42,
+      width: 4,
+      height: 5
+    },
+    {
+      src: posse43,
+      width: 6,
+      height: 5
+    },
+    {
+      src: posse44,
+      width: 4,
+      height: 5
+    },
+    {
+      src: posse45,
+      width: 6,
+      height: 5
+    },
+    {
+      src: posse46,
+      width: 2,
+      height: 3
+    },
+    {
+      src: posse47,
+      width: 4,
+      height: 3
+    },
+    {
+      src: posse48,
+      width: 7,
+      height: 5
+    },
+    {
+      src: posse49,
+      width: 4,
+      height: 4
+    },
+    {
+      src: posse50,
+      width: 4,
+      height: 4
+    },
+    {
+      src: posse51,
+      width: 4,
+      height: 3
+    },
+    {
+      src: posse52,
+      width: 3,
+      height: 4
+    },
+    {
+      src: posse53,
+      width: 4,
+      height: 3
+    },
+    {
+      src: posse54,
+      width: 3,
+      height: 2
+    },
+    {
+      src: posse55,
+      width: 4,
+      height: 4
+    },
+    {
+      src: posse56,
+      width: 4,
+      height: 3
+    },
+    {
+      src: posse57,
+      width: 4,
+      height: 3
+    },
+    {
+      src: posse58,
+      width: 3,
+      height: 2
+    },
+    {
+      src: posse59,
+      width: 16,
+      height: 9
+    },
+    {
+      src: posse60,
+      width: 4,
+      height: 4
+    }
+  ];
+  
